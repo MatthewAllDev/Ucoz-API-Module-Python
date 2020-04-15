d33fe66f2b22ace9e7e1bb67c42fb26083b36fe7
MatthewAllDev <MatthewAllDev@users.noreply.github.com>
2020-04-15 16:15:20 +0300

    chore, refactor and style
    1. Implemented:  Added setting of transfer protocol to class attributes
    2. Refactoring and styling code

--- Examples module work.py
+++ Examples module work.py
@@ -1,21 +1,23 @@
-#Examples of requests using the UAPI module. As an example, the interaction with the  module "Site News" is presented.
-#author Ilya Matthew Kuvarzin <luceo2011@yandex.ru>
-#version 1.0 dated October 17, 2018
-#Return_value is dect
+# Examples of requests using the UAPI module. As an example, the interaction with the  module "Site News" is presented.
+# author Ilya Matthew Kuvarzin <luceo2011@yandex.ru>
+# version 1.1 dated April 15, 2020
+# Return_value is dict
 
 import uAPI
 
-request = uAPI.Request('mysite.com',{
-    'oauth_consumer_key': 'Application id',
-    'oauth_consumer_secret': 'Consumer secret',
-    'oauth_token': 'OAuth token',
-    'oauth_token_secret': 'OAuth token secret'
-})
+request = uAPI.Request('my-site.com', 'https',
+                       {
+                           'oauth_consumer_key': 'Application id',
+                           'oauth_consumer_secret': 'Consumer secret',
+                           'oauth_token': 'OAuth token',
+                           'oauth_token_secret': 'OAuth token secret'
+                       })
 
 return_value = request.get('/news', {})
 print(return_value)
 
-return_value = request.post('/news', {'category': 'CATEGORY-ID', 'title': 'Material name', 'message': 'This is a test material. Request succeeded.'})
+return_value = request.post('/news', {'category': 'CATEGORY-ID', 'title': 'Material name',
+                                      'message': 'This is a test material. Request succeeded.'})
 print(return_value)
 
 return_value = request.put('/news', {'id': 'MATERIAL-ID', 'message': 'Material successfully changed'})
--- uAPI.py
+++ uAPI.py
@@ -1,6 +1,6 @@
-#Set of methods for requesting the uCoz API
-#author Ilya Matthew Kuvarzin <luceo2011@yandex.ru>
-#version 1.0 dated October 17, 2018
+# Python module for easy integration with uAPI (ucoz API).
+# author Ilya Matthew Kuvarzin <luceo2011@yandex.ru>
+# version 1.1 dated April 15, 2020
 
 from urllib.parse import quote_plus, urlencode
 from time import time
@@ -11,10 +11,12 @@ import hmac
 from base64 import b64encode
 import requests
 
+
 class Request(object):
 
-    def __init__(self, site, config):
+    def __init__(self, site, transfer_protocol, config):
         self.site = site
+        self.transfer_protocol = transfer_protocol
         self.config = config
         self.params = {
             'oauth_version': '1.0',
@@ -25,44 +27,52 @@ class Request(object):
             'oauth_token': self.config['oauth_token']
         }
 
-    def __getSignature(self, method, url, params):
-        baseString = method.upper() + '&' + quote_plus(url) + '&' + quote_plus(Request.__http_build_query(params).replace('+', '%20'))
-        return quote_plus(b64encode(Request.__hash_hmac('sha1', baseString, self.config['oauth_consumer_secret'] + '&' + self.config['oauth_token_secret'], True)).decode("utf-8"))
+    def __get_signature(self, method, url, params):
+        base_string = method.upper() + '&' + quote_plus(url) + '&' \
+                      + quote_plus(Request.__http_build_query(params).replace('+', '%20'))
+        return quote_plus(b64encode(Request.__hash_hmac('sha1', base_string, self.config['oauth_consumer_secret'] + '&'
+                                                        + self.config['oauth_token_secret'], True)).decode("utf-8"))
 
+    @staticmethod
     def __http_build_query(params):
         return urlencode(dict(sorted(params.items())))
 
-    def __hash_hmac(algo, data, key, raw_output=False):
+    @staticmethod
+    def __hash_hmac(algorithm, data, key, raw_output=False):
         if raw_output:
-            res = hmac.new(key.encode(), data.encode(), algo).digest()
+            res = hmac.new(key.encode(), data.encode(), algorithm).digest()
         else:
-            res = hmac.new(key.encode(), data.encode(), algo).hexdigest()
+            res = hmac.new(key.encode(), data.encode(), algorithm).hexdigest()
         return res
 
     def get(self, url, data=None):
         self.params['oauth_nonce'] = md5(str(str(time()) + str(randint(0, maxsize))).encode('utf-8')).hexdigest()
-        url = 'http://' + self.site + '/uapi' + url
-        queryString = Request.__http_build_query(dict(self.params, **data, **{'oauth_signature': self.__getSignature('get', url, dict(self.params, **data))}))
-        request = url + '?' + queryString
+        url = self.transfer_protocol + '://' + self.site + '/uapi' + url
+        query_string = Request.__http_build_query(dict(self.params, **data, **{
+            'oauth_signature': self.__get_signature('get', url, dict(self.params, **data))}))
+        request = url + '?' + query_string
         response = requests.get(request).json()
         return response
 
     def post(self, url, data):
         self.params['oauth_nonce'] = md5(str(str(time()) + str(randint(0, maxsize))).encode('utf-8')).hexdigest()
-        url = 'http://' + self.site + '/uapi' + url
-        response = requests.post(url, data=dict(self.params, **data, **{'oauth_signature': self.__getSignature('post', url, dict(self.params, **data))})).json()
+        url = self.transfer_protocol + '://' + self.site + '/uapi' + url
+        response = requests.post(url, data=dict(self.params, **data, **{
+            'oauth_signature': self.__get_signature('post', url, dict(self.params, **data))})).json()
         return response
 
     def put(self, url, data):
         self.params['oauth_nonce'] = md5(str(str(time()) + str(randint(0, maxsize))).encode('utf-8')).hexdigest()
-        url = 'http://' + self.site + '/uapi' + url
-        response = requests.put(url, data=dict(self.params, **data, **{'oauth_signature': self.__getSignature('put', url, dict(self.params, **data))})).json()
+        url = self.transfer_protocol + '://' + self.site + '/uapi' + url
+        response = requests.put(url, data=dict(self.params, **data, **{
+            'oauth_signature': self.__get_signature('put', url, dict(self.params, **data))})).json()
         return response
 
     def delete(self, url, data):
         self.params['oauth_nonce'] = md5(str(str(time()) + str(randint(0, maxsize))).encode('utf-8')).hexdigest()
-        url = 'http://' + self.site + '/uapi' + url
-        queryString = Request.__http_build_query(dict(self.params, **data, **{'oauth_signature': self.__getSignature('delete', url, dict(self.params, **data))}))
-        request = url + '?' + queryString
+        url = self.transfer_protocol + '://' + self.site + '/uapi' + url
+        query_string = Request.__http_build_query(dict(self.params, **data, **{
+            'oauth_signature': self.__get_signature('delete', url, dict(self.params, **data))}))
+        request = url + '?' + query_string
         response = requests.delete(request).json()
         return response
